da83628fe1fef7940c4a5ee071cdb5ae6804b14a
Aaron Tran <dahpaladin123@gmail.com>
2024-06-20 20:37:57 -0400

    added test main.py and made face rec into funct

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from PIL import Image
+import numpy as np
+from facial_recognition import recognize_faces
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {device}")
+
+# Load the models
+mtcnn = MTCNN(image_size=160, margin=20, keep_all=True, device=device)  # Increased margin
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+def get_face_embedding(face):
+    return resnet(face.to(device)).detach()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    recognize_faces(frame)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
