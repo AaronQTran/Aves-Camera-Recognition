fb81c1d605a4bc717d932f195ff9d44b7b26ea37
AaronQTran <dahpaladin123@gmail.com>
2024-06-29 02:09:27 -0400

    s

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
 import { useCookies } from 'react-cookie';
 import { Routes, Route } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ const Main = () => {
       {showSplash && (
         <div className="splash-screen absolute inset-0 flex items-center justify-center z-50 gradient-background">
           <h1 className="text-white text-4xl">Aves Attendance</h1>
+          <PendingActionsIcon className = "ml-2" style={{ fontSize: 50 }}/>
         </div>
       )}
       <div className="w-1/4 h-full border-r border-white-200">
